c2b41d76124f14f87c7b754aad7e417bb5a92470
fkaempf <florianfelixkampf@gmail.com>
2025-05-30 20:40:30 +0100

    Refactor image scraping to handle multiple sheets and improve download logic

--- scrape.images.py
+++ scrape.images.py
@@ -16,26 +16,52 @@ creds = Credentials.from_service_account_file(cred_path, scopes=scope)
 client = gspread.authorize(creds)
 
 # Open your spreadsheet
-sheet = client.open("13398.matching.sheet").worksheet('full').get_all_records()
+sheet1 = client.open("13398.matching.sheet").worksheet('full').get_all_records()
+sheet2 = client.open("12383.matching.sheet").worksheet('full').get_all_records()
+sheet3 = client.open("12425.matching.sheet").worksheet('full').get_all_records()
 
 # Example usage
-df = pd.DataFrame(sheet)
+df1 = pd.DataFrame(sheet1)
+df2 = pd.DataFrame(sheet2)
+df3 = pd.DataFrame(sheet3)
+df = pd.concat([df1, df2, df3], ignore_index=True)
+
 
 pre = 'https://s3.amazonaws.com/janelia-flylight-color-depth'
 
 os.makedirs('target', exist_ok=True)
-for i,item in tqdm(df.iterrows(),total=df.shape[0]):
-    url = f'{pre}/{item['Alignment Space']}/{item['Library'].replace(' ','_')}/searchable_neurons/pngs/{item['Line Name']}-{item['Slide Code']}-Split_GAL4-{item['Sex']}-{item['Magnification']}-{item['Anatomical Area'].lower()}-{item['Alignment Space']}-CDM_1-01.png'
-    
-    if not f'{url.split('/')[-1]}' in os.listdir('target'):
-        response = requests.get(url)
-        if response.status_code == 200:
-            with open(f'target/{url.split('/')[-1]}', 'wb') as f:
-                f.write(response.content)
-    
-        else:
-            url = f'{pre}/{item['Alignment Space']}/{item['Library'].replace(' ','_')}/searchable_neurons/pngs/{item['Line Name']}-{item['Slide Code']}-GAL4-{item['Sex']}-{item['Magnification']}-{item['Anatomical Area'].lower()}-{item['Alignment Space']}-CDM_1-01.png'
-            response = requests.get(url)
+content_target = os.listdir('target')
+for i, item in tqdm(df.iterrows(), total=df.shape[0]):
+    error404 = True
+    found = False
+    for img_no in range(1, 10):
+        img_no = f"{img_no:02d}"
+        for Gal4 in ['Split_GAL4', 'GAL4']:
+            url = f"{pre}/{item['Alignment Space']}/{item['Library'].replace(' ','_')}/searchable_neurons/pngs/{item['Line Name']}-{item['Slide Code']}-{Gal4}-{item['Sex']}-{item['Magnification']}-{item['Anatomical Area'].lower()}-{item['Alignment Space']}-CDM_{item['Channel']}-{img_no}.png"
+            filename = url.split('/')[-1]
+            if filename in content_target:
+                error404 = False
+                found = True
+                break
+            else:
+                response = requests.get(url)
+                if response.status_code == 200:
+                    #print(f"Downloading {filename}")
+                    with open(f'target/{filename}', 'wb') as f:
+                        f.write(response.content)
+                    error404 = False
+                    found = True
+                    break
+        if found:
+            break
+    if error404:
+        print(f"\033[91mFile not found: {filename}\033[0m")
 
-    else:
-        print(f'{url.split("/")[-1]} already exists, skipping download.')
+
+    
+ 
+
+
+
+    
+
